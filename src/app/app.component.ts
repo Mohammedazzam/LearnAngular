f9bc8d2ec4d4b03f34ce6f07e1bb19ffdefc4e18
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2023-07-05 15:02:42 +0300

    edit component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CourseDemo';
+  title = 'Courses Front-End Angular';
+  sayMohammed():string {
+    return `Hello Mohammed Azzam!`;
+  }
 }
